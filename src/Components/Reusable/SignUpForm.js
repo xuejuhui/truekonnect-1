14942058e28f66ddf69336044c72762740f7dc2c
Chris Jannenga <cjannenga312@gmail.com>
2018-05-05 11:57:07 -0700

    Changed sign-up form to contain react-strap materials and removed MDL

--- src/Components/Reusable/SignUpForm.js
+++ src/Components/Reusable/SignUpForm.js
@@ -2,70 +2,53 @@ import React, {Component} from "react";
 import firebase from 'firebase';
 import axios from 'axios';
 
+import {Form, FormGroup, Label, Input} from 'reactstrap';
+
 class SignUpForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: '',
-            password: ''
-        };
-    }
-
-    onButtonPress() {
-        const {email, password} = this.state;
-
-        if (email === '' || password === '') {
-            return alert('Must fill in all fields')
-        }
-        return (
-            firebase.auth().createUserWithEmailAndPassword(email, password)
-        // .then(() => axios.post('http://localhost:3000/users/new', {
-        //     email: email,
-        //     user_token: firebase.auth().currentUser.uid,
-        // }))
-        // .then(() => console.log('this works')))
-        )
-    }
-
-    handleEmailTextChange = (event) => {
-        this.setState({email: event.target.value})
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
     };
+  }
 
-    handlePassTextChange = (event) => {
-        this.setState({password: event.target.value})
-    };
+  onButtonPress() {
+    const {email, password} = this.state;
 
-    render() {
-        return (
-            <form className="basicForm" action="#">
-                <div className="mdl-textfield mdl-js-textfield">
-                    <input
-                        className="mdl-textfield__input"
-                        type="text"
-                        onChange={this.handleEmailTextChange}
-                        value={this.state.email}>
-                    </input>
-                    <label className="mdl-textfield__label">Email</label>
-                </div>
-                <br/>
-                <div className="mdl-textfield mdl-js-textfield">
-                    <input
-                        className="mdl-textfield__input"
-                        type="password"
-                        onChange={this.handlePassTextChange}
-                        value={this.state.password}>
-                    </input>
-                    <label className="mdl-textfield__label">Password</label>
-                </div>
-                <br/>
-                <button
-                    className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored"
-                    onClick={() => this.onButtonPress()}>
-                    Submit
-                </button>
-            </form>
-        );
-    }
+  //   if (email === '' || password === '') {
+  //     return alert('Must fill in all fields')
+  //   }
+  //   return (firebase.auth().createUserWithEmailAndPassword(email, password)
+  //   // .then(() => axios.post('http://localhost:3000/users/new', {
+  //   //     email: email,
+  //   //     user_token: firebase.auth().currentUser.uid,
+  //   // }))
+  //   // .then(() => console.log('this works'))))
+}
+
+  handleEmailTextChange = (event) => {
+    this.setState({email: event.target.value})
+  };
+
+  handlePassTextChange = (event) => {
+    this.setState({password: event.target.value})
+  };
+
+  render() {
+    return (
+      <Form>
+        <FormGroup>
+          <Label for="exampleEmail">Email</Label>
+          <Input type="email" name="email" id="exampleEmail" onChange={this.handleEmailTextChange} value={this.state.email} placeholder="with a placeholder"/>
+        </FormGroup>
+        <FormGroup>
+          <Label for="examplePassword">Password</Label>
+          <Input type="password" name="password" id="examplePassword" onChange={this.handlePassTextChange} value={this.state.password} placeholder="password placeholder"/>
+        </FormGroup>
+      </Form>
+    );
+  }
 }
 
 export default SignUpForm;
