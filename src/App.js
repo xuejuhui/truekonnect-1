48f522178a9be99782875b86b513a55440bc2bf1
Chris Jannenga <cjannenga312@gmail.com>
2018-05-05 12:28:15 -0700

    Changed basic component to add sizing properties

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
 import SignUpForm from "./Components/Reusable/SignUpForm";
+import StylistCard from './Components/Reusable/StylistCard';
 
 class App extends Component {
   render() {
     return (
         <div className="App">
           <SignUpForm/>
+          <StylistCard />
         </div>);
   }
 }
--- src/Components/Reusable/StylistCard.js
+++ src/Components/Reusable/StylistCard.js
@@ -1,8 +1,8 @@
-import react from 'react'
+import React from 'react'
 
-import {Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button} from 'reactstrap'
+import {Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button, Row, Col} from 'reactstrap'
 
-class StylistCard extends React.Compnent {
+class StylistCard extends React.Component {
   constructor(props) {
     super(props);
     this.state= {}
@@ -10,17 +10,22 @@ class StylistCard extends React.Compnent {
 
   render () {
     return (
-      <div>
-      <Card>
-        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
-        <Button>Button</Button>
-        </CardBody>
-      </Card>
-    </div>
+      <Row>
+      <Col sm="6">
+        <Card body>
+          <CardTitle>Special Title Treatment</CardTitle>
+          <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+          <Button>Go somewhere</Button>
+        </Card>
+      </Col>
+      <Col sm="6">
+        <Card body>
+          <CardTitle>Special Title Treatment</CardTitle>
+          <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+          <Button>Go somewhere</Button>
+        </Card>
+      </Col>
+    </Row>
     )
   }
 }
