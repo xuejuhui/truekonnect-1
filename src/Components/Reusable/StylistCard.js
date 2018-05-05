dcc692d552af679d865691f9c8c21ccfdef8dc74
Chris Jannenga <cjannenga312@gmail.com>
2018-05-05 12:23:37 -0700

    Created Stylist Card Base Structure

---
+++ src/Components/Reusable/StylistCard.js
@@ -0,0 +1,28 @@
+import react from 'react'
+
+import {Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button} from 'reactstrap'
+
+class StylistCard extends React.Compnent {
+  constructor(props) {
+    super(props);
+    this.state= {}
+  }
+
+  render () {
+    return (
+      <div>
+      <Card>
+        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+        <CardBody>
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
+        <Button>Button</Button>
+        </CardBody>
+      </Card>
+    </div>
+    )
+  }
+}
+
+export default StylistCard
