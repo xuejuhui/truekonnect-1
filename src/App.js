04c3c311bce957da5661c097f27f84996d4ca2ca
Chris Jannenga <cjannenga312@gmail.com>
2018-05-05 12:15:49 -0700

    Removed checkout component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
 import SignUpForm from "./Components/Reusable/SignUpForm";
-import Checkout from './Components/Reusable/Checkout';
 
 class App extends Component {
   render() {
