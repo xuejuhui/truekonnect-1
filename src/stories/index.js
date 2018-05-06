449579bf9285167665c13b3269bc064afdbf3a8a
Chris Jannenga <cjannenga312@gmail.com>
2018-05-06 14:00:51 -0700

    storybook

---
+++ src/stories/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+
+
+import SignUpForm from '../Components/Reusable/SignUpForm';
+
+import StylistCard from '../Components/Reusable/StylistCard';
+
+
+
+storiesOf('Login', module)
+  .add('Login Boxes', () => <SignUpForm />);
+
+storiesOf('Cards', module)
+  .add('Stylist Card', () => <StylistCard StylistName='Chris Jannenga' StylistLocation='Phoenix, AZ' />)
