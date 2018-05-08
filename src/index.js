d96ace3075a7a2f96865261731b0130fb9c06465
Ismael Perez <ismaelperezemail@gmail.com>
2018-05-07 17:52:01 -0700

    SignInForm: Changed file name from SignUpForm to SignInForm

--- src/index.js
+++ src/index.js
@@ -8,14 +8,14 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 
 var config = {
-  apiKey: "AIzaSyAPolGXOq3CFjHYlmCNZAEWGz7kpi-4OtA",
-  authDomain: "true-konnect-mvp.firebaseapp.com",
-  databaseURL: "https://true-konnect-mvp.firebaseio.com",
-  projectId: "true-konnect-mvp",
-  storageBucket: "",
-  messagingSenderId: "776189930808"
+    apiKey: "AIzaSyAPolGXOq3CFjHYlmCNZAEWGz7kpi-4OtA",
+    authDomain: "true-konnect-mvp.firebaseapp.com",
+    databaseURL: "https://true-konnect-mvp.firebaseio.com",
+    projectId: "true-konnect-mvp",
+    storageBucket: "true-konnect-mvp.appspot.com",
+    messagingSenderId: "776189930808"
 };
 firebase.initializeApp(config);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
