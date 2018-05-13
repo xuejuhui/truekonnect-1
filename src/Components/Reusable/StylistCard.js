337bbfee19851154e1e4ae10c63660960ef4d8cd
PocketFreud <jeremy@phxconscious.com>
2018-05-12 17:55:15 -0700

    Content added, and made updates to align & space elements correctly

--- src/Components/Reusable/StylistCard.js
+++ src/Components/Reusable/StylistCard.js
@@ -17,20 +17,59 @@ class StylistCard extends React.Component {
         <Card>
         <CardImg top width='100%' src='http://via.placeholder.com/200x200' />
         <CardBody>
-          <Row>
+          <Row className="clearfix">
             <Col sm='2'>
-              <Media object src="http://via.placeholder.com/64x64"  className ='rounded-circle' alt="Generic placeholder image" />
+              <Media object src="http://via.placeholder.com/64x64"  className ='rounded-circle user-profile-image mx-auto' alt="Generic placeholder image" />
             </Col>
-            <Col sm='6'>
-              <CardTitle className='align-middle'>{this.props.StylistName}</CardTitle>
+            <Col sm='8' className="inline">
+              <CardTitle className="align-middle py-4">{this.props.StylistName}</CardTitle>
             </Col>
-            <Col sm={{size:1, offset:3}}>
-              <i className="fas fa-ellipsis-h 2x"></i>
+            <Col sm='1' className="float-right">
+              <i className="fas fa-ellipsis-h"></i>
             </Col>
           </Row>
-          <CardText>{this.props.StylistLocation}</CardText>
-          <CardText ClassName="user-description-text">vacations</CardText>
-          <Button>Go somewhere</Button>
+          <Row>
+            <Col sm='2' className="py-1">
+            <i className="fas fa-cut dark-grey user-description-icon"></i>
+            </Col>
+            <Col sm="10" className="py-1">
+            <CardText className="user-description-text">Extensions, Chemical Perms, Ethnic Hair{this.props.StylistSpecialty}</CardText>
+            </Col>
+          </Row>
+          <Row>
+            <Col sm='2' className="py-1">
+            <i className="fas fa-id-card dark-grey user-description-icon"></i>
+            </Col>
+            <Col sm="10" className="py-1">
+            <CardText className="user-description-text">6-10{this.props.StylistExperience} years of experience</CardText>
+            </Col>
+          </Row>
+          <Row>
+            <Col sm='2' className="py-1">
+            <i className="fas fa-church dark-grey user-description-icon"></i>
+            </Col>
+            <Col sm="10" className="py-1">
+            <CardText className="user-description-text">Spiritual/Other{this.props.StylistReligion}</CardText>
+            </Col>
+          </Row>
+          <Row>
+            <Col sm="12">
+              <CardText className="user-card-section-title">Bio</CardText>
+            </Col>
+          </Row>
+          <Row>
+            <Col sm="12">
+              <CardText className="user-bio">Founder of TrueKonnect, where we connect you to your ideal hairstylist. Empowering female entrepreneurs to live the way we were designed to be, PRETTY.</CardText>
+            </Col>
+          </Row>
+          <Row>
+            <Col sm={{size:1, offset:4}}>
+              <Button outline color="secondary" className="rounded-circle vibe-or-not-buttons"><i className="fas fa-times dark-grey"></i></Button>
+            </Col>
+            <Col sm={{size:1, offset:1}}>
+              <Button outline color="danger" className="rounded-circle vibe-or-not-buttons"><i className="fas fa-heart red"></i></Button>
+            </Col>
+          </Row>
         </CardBody>
         </Card>
       </Col>
