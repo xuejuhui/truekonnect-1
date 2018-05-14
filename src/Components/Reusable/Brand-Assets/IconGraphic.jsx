622fbf750d2eb57dfd817ac7ae76b9162969c9fa
Chris Jannenga <cjannenga312@gmail.com>
2018-05-14 01:15:56 -0700

    Added Icon Graphic Story and Components

---
+++ src/Components/Reusable/Brand-Assets/IconGraphic.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import './icon-graphic.css'
+
+import {} from 'reactstrap'
+
+class IconGraphic extends Component {
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default IconGraphic
