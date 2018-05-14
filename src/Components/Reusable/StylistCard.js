1634f96b2e6affd4eab839c46215cad7cbdc35c0
Chris Jannenga <cjannenga312@gmail.com>
2018-05-13 23:26:37 -0700

    Added jeremy's updates

--- src/Components/Reusable/StylistCard.js
+++ src/Components/Reusable/StylistCard.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import './stylist-card.css';
 
-import {Container, Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button, Row, Col, Media} from 'reactstrap'
+import {Container, Card, CardImg, CardText, CardBody, CardTitle, Button, Row, Col, Media} from 'reactstrap'
 
 class StylistCard extends React.Component {
   constructor(props) {
@@ -13,22 +14,58 @@ class StylistCard extends React.Component {
       <Container>
       <Row>
       <Col sm="10" md='8' lg='6'>
-        <Card>
-        <CardImg top width='100%' src='http://via.placeholder.com/200x200' />
+        <Card className="shadow">
+        <CardImg top width='100%' src='http://via.placeholder.com/400x400' />
         <CardBody>
-          <Row>
+          <Row className="clearfix pb-3">
             <Col sm='2'>
-              <Media object src="http://via.placeholder.com/64x64"  className ='rounded-circle' alt="Generic placeholder image" />
+              <Media object src="http://via.placeholder.com/42x42"  className ='rounded-circle user-profile-image' alt="Generic placeholder image" />
             </Col>
-            <Col sm='6'>
-              <CardTitle className='align-middle'>{this.props.StylistName}</CardTitle>
+            <Col sm='9' className="align-self-center">
+              <CardTitle className="align-self-center">Teresa Young{/*this.props.StylistName*/}</CardTitle>
             </Col>
-            <Col sm={{size:1, offset:3}}>
-              <i className="fas fa-ellipsis-h 2x"></i>
+            <Col sm='1' className="float-right d-flex align-items-center justify-content-center">
+              <i className="fas fa-ellipsis-h fa-lg"></i>
             </Col>
           </Row>
-          <CardText>{this.props.StylistLocation}</CardText>
-          <Button>Go somewhere</Button>
+          <Row>
+            <Col sm='2' className="py-1 d-flex align-items-center justify-content-center">
+              <i className="fas fa-cut fa-lg dark-grey"></i>
+            </Col>
+            <Col sm="10" className="py-1">
+            <CardText className="user-description-text">Extensions, Chemical Perms, Ethnic Hair{this.props.StylistSpecialty}</CardText>
+            </Col>
+          </Row>
+          <Row>
+            <Col sm='2' className="py-1 d-flex align-items-center justify-content-center">
+              <i className="fas fa-id-card fa-lg dark-grey"></i>
+            </Col>
+            <Col sm="10" className="py-1">
+            <CardText className="user-description-text">11+{this.props.StylistExperience} years of experience</CardText>
+            </Col>
+          </Row>
+          <Row>
+            <Col sm='2' className="py-1 d-flex align-items-center justify-content-center">
+              <i className="fas fa-church fa-lg dark-grey"></i>
+            </Col>
+            <Col sm="10" className="py-1">
+            <CardText className="user-description-text">Christian{this.props.StylistReligion}</CardText>
+            </Col>
+          </Row>
+          {/* <Row className="pt-2">
+            <Col sm="12">
+              <CardText className="user-card-section-title">Bio</CardText>
+            </Col>
+          </Row> */}
+          <Row className="py-3">
+            <Col sm="12">
+              <CardText className="user-bio">Founder of TrueKonnect, where we connect you to your ideal hairstylist. Empowering female entrepreneurs to live the way we were designed to be, PRETTY.</CardText>
+            </Col>
+          </Row>
+          <Row className="d-flex justify-content-center">
+              <Button outline color="secondary" className="rounded-circle vibe-or-not-buttons"><i className="fas fa-times dark-grey"></i></Button>
+              <Button outline color="danger" className="rounded-circle vibe-or-not-buttons"><i className="fas fa-heart red"></i></Button>
+          </Row>
         </CardBody>
         </Card>
       </Col>
