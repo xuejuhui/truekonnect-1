caef06e90b5ba8928aae1c4da38552ea23bb5c89
Chris Jannenga <cjannenga312@gmail.com>
2018-05-14 19:59:32 -0700

    Added Age emoji structures

---
+++ src/Components/Reusable/Emoji-Icons/Age/ThreeCandles.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import '../emoji.css'
+
+import {} from 'reactstrap'
+
+class ThreeCandles extends Component {
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default ThreeCandles
