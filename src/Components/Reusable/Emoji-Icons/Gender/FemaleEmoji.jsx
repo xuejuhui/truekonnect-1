9908204c4bbfe50fd775f6f049d3f10014e49716
Chris Jannenga <cjannenga312@gmail.com>
2018-05-16 22:08:58 -0700

    Added Female, Male, LGBTQ+, Client, and Stylist Emoji components and stories

---
+++ src/Components/Reusable/Emoji-Icons/Gender/FemaleEmoji.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import '../emoji.css'
+
+import {} from 'reactstrap'
+
+class FemaleEmoji extends Component {
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default FemaleEmoji
---
+++ src/Components/Reusable/Emoji-Icons/User-Types/ClientEmoji.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import '../emoji.css'
+
+import {} from 'reactstrap'
+
+class ClientEmoji extends Component {
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default ClientEmoji
